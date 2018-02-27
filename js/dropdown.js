72ea8339ba06853cdfcf17492ef2bf6db3f9bd8f
Unknown <memischap@me.com>
2018-02-26 18:33:23 -0800

    FREAKING JS WORKS !!!!!

--- js/dropdown.js
+++ js/dropdown.js
@@ -3,47 +3,34 @@ var menuButton = document.querySelector("#menu-button");
 var navItems = document.querySelector("#main-nav");
 var width = window.innerWidth;
 
-function resize(){
-  width = window.innerWidth;
-  console.log(width);
-}
 
-window.onresize = resize;
-// window.addEventListener("resize",function(){
-//   width = window.innerWidth;
-//
-//
-//
-//
-// if(width < 768){
-//   console.log("break point menu button activated");
-//   menuButton.classList.remove("hidden");
-//   navItems.classList.add("hidden");
 
-// menuButton.addEventListener("click",function(){
-//   console.log("clicked a button");
-//   if(navItems.classList.contains("hidden")){
-//     console.log("hide the nav");
-//     navItems.classList.remove("hidden");
-//     navItems.setAttribute("aria-hidden","true");
-//   }else{
-//     console.log("show the nav");
-//     navItems.classList.add("hidden");
-//     navItems.setAttribute("aria-hidden","false");
-//   }
-// })
-// }else{
-//   menuButton.classList.add("hidden");
-//   navItems.classList.remove("hidden");
+
+// updateWitdth("resize");
+// function updateWitdth(){
+//   window.addEventListener("resize",function(){
+//     width = window.innerWidth;
+//   })
+//
+//
 // }
-// return width = width;
-// })
+// console.log(width);
 
+
+window.addEventListener("resize",function(){
+  width = window.innerWidth;
 
 if(width < 768){
-  console.log("second width selector");
+  console.log("break point menu button activated");
   menuButton.classList.remove("hidden");
   navItems.classList.add("hidden");
+
+}else{
+  menuButton.classList.add("hidden");
+  navItems.classList.remove("hidden");
+}
+
+})
 
 menuButton.addEventListener("click",function(){
   console.log("clicked a button");
@@ -57,6 +44,14 @@ menuButton.addEventListener("click",function(){
     navItems.setAttribute("aria-hidden","false");
   }
 })
+
+console.log(width);
+if(width < 768){
+  console.log("second width selector");
+  menuButton.classList.remove("hidden");
+  navItems.classList.add("hidden");
+
+
 }else{
   menuButton.classList.add("hidden");
   navItems.classList.remove("hidden");
